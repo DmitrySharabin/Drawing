cedc4848063cbb5a2dd71f0aecf7f79ea6ab56a5
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-11-17 11:56:05 +0300

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Dmitry Sharabin on 17.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
