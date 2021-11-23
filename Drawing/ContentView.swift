159f37529fbb5a6d9454c0b039e11117ddee41a1
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-11-23 22:46:25 +0300

    Day 45 — Special effects in SwiftUI: blurs, blending, and more

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,52 +7,62 @@
 
 import SwiftUI
 
-struct ColorCyclingCircle: View {
-    var amount = 0.0
-    var steps = 100
-    
-    var body: some View {
-        ZStack {
-            ForEach(0..<steps) { value in
-                Circle()
-                    .inset(by: Double(value))
-//                    .strokeBorder(color(for: value, brightness: 1), lineWidth: 2)
-                    .strokeBorder(
-                        LinearGradient(
-                            gradient: Gradient(colors: [
-                                color(for: value, brightness: 1),
-                                color(for: value, brightness: 0.5)
-                            ]),
-                            startPoint: .top,
-                            endPoint: .bottom
-                        ),
-                        lineWidth: 2
-                    )
-            }
-        }
-        .drawingGroup()
-    }
-    
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(steps) + amount
-        
-        if targetHue > 1 {
-            targetHue -= 1
-        }
-        
-        return Color(hue: targetHue, saturation: 1, brightness: brightness)
-    }
-}
-
 struct ContentView: View {
-    @State private var colorCycle = 0.0
+    @State private var amount = 0.0
     
     var body: some View {
+//        ZStack{
+//            Image("Example")
+//                .colorMultiply(.red)
+//
+////            Rectangle()
+////                .fill(.red)
+////                .blendMode(.multiply)
+//        }
+//        .frame(width: 400, height: 500)
+//        .clipped()
+        
+//        VStack {
+//            ZStack {
+//                Circle()
+////                    .fill(.red)
+//                    .fill(Color(red: 1, green: 0, blue: 0))
+//                    .frame(width: 200 * amount)
+//                    .offset(x: -50, y: -80)
+//                    .blendMode(.screen)
+//
+//                Circle()
+////                    .fill(.green)
+//                    .fill(Color(red: 0, green: 1, blue: 0))
+//                    .frame(width: 200 * amount)
+//                    .offset(x: 50, y: -80)
+//                    .blendMode(.screen)
+//
+//                Circle()
+////                    .fill(.blue)
+//                    .fill(Color(red: 0, green: 0, blue: 1))
+//                    .frame(width: 200 * amount)
+//                    .blendMode(.screen)
+//            }
+//            .frame(width: 300, height: 300)
+//
+//            Slider(value: $amount)
+//                .padding()
+//        }
+//        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//        .background(.black)
+//        .ignoresSafeArea()
+        
         VStack {
-            ColorCyclingCircle(amount: colorCycle)
-                .frame(width: 300, height: 300)
+            Image("Example")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .saturation(amount)
+                .blur(radius: (1 - amount) * 20)
             
-            Slider(value: $colorCycle)
+            Slider(value: $amount)
+                .padding()
         }
     }
 }
